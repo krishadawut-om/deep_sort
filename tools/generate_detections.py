180743516431ccd0acbc9a80257661935c3d6375
Krishadawut Olde Monnikhof <78374713+krishadawut-om@users.noreply.github.com>
2022-12-24 21:10:20 +0100

    Update generate_detections.py

--- tools/generate_detections.py
+++ tools/generate_detections.py
@@ -73,7 +73,7 @@ class ImageEncoder(object):
     def __init__(self, checkpoint_filename, input_name="images",
                  output_name="features"):
         self.session = tf.compat.v1.Session()
-        with tf.gfile.GFile(checkpoint_filename, "rb") as file_handle:
+        with tf.io.gfile.GFile(checkpoint_filename, "rb") as file_handle:
             graph_def = tf.GraphDef()
             graph_def.ParseFromString(file_handle.read())
         tf.import_graph_def(graph_def, name="net")
