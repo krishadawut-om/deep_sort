2ff37bb1b8f9257a7cd330b2a7735e6b6ba5a4d8
Krishadawut Olde Monnikhof <78374713+krishadawut-om@users.noreply.github.com>
2022-12-24 22:35:44 +0100

    Update tracker.py

--- deep_sort/tracker.py
+++ deep_sort/tracker.py
@@ -95,6 +95,9 @@ class Tracker:
         def gated_metric(tracks, dets, track_indices, detection_indices):
             features = np.array([dets[i].feature for i in detection_indices])
             targets = np.array([tracks[i].track_id for i in track_indices])
+            print(features)
+            print(targets)
+            
             cost_matrix = self.metric.distance(features, targets)
             cost_matrix = linear_assignment.gate_cost_matrix(
                 self.kf, cost_matrix, tracks, dets, track_indices,
